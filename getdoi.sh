bd2bc8df783e543c10dbc9f8cf44f557deecffb9
fd <fdecunta@agro.uba.ar>
2025-09-17 11:53:07 -0300

    add script

---
+++ getdoi.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+usage() {
+    echo "Usage:  doi [REFERENCE]"
+    echo "    [REFERENCE]  Paper to search in CrossRef"
+}
+
+if [ ! $1 ]; then
+    usage
+    exit
+fi
+
+my_query=$(echo "$1" | jq -sRr @uri)
+curl "https://api.crossref.org/works?query=$my_query" | jq '.message.items[0].DOI'
